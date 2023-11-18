d1f6341688b11e964ccb3cfb0ec8cbaaf5615409
vasko34 <56612508+vasko34@users.noreply.github.com>
2023-11-18 16:01:26 +0200

    c

--- Project21/ViewController.swift
+++ Project21/ViewController.swift
@@ -1,19 +1,91 @@
-//
-//  ViewController.swift
-//  Project21
-//
-//  Created by User on 8.11.23.
-//
-
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
     }
-
-
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yay!")
+            } else {
+                print("Shit!")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal(reminder: Bool) {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        registerCategories()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Title goes here"
+        content.body = "Main text goes here"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["DataToSend": "Data"]
+        content.sound = .default
+        
+        if reminder {
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 86400, repeats: false)
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        } else if reminder != true {
+            var dateComponents = DateComponents()
+            dateComponents.hour = 10
+            dateComponents.minute = 30
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            //let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+    }
+    
+    func registerCategories() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        
+        let show = UNNotificationAction(identifier: "show", title: "Tell me more...", options: .foreground)
+        let showReminder = UNNotificationAction(identifier: "showReminder", title: "Remind me later", options: .foreground)
+        let category = UNNotificationCategory(identifier: "alarm", actions: [show, showReminder], intentIdentifiers: [])
+        
+        center.setNotificationCategories([category])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
+        
+        if let dataToSend = userInfo["DataToSend"] as? String {
+            print("Data received: \(dataToSend)")
+            
+            switch response.actionIdentifier {
+            case UNNotificationDefaultActionIdentifier:
+                let ac = UIAlertController(title: "Default identifier", message: "The user swiped to unlock the phone.", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            case "show":
+                let ac = UIAlertController(title: "Custom show identifier", message: "The user tapped the Tell me more... button", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            case "showReminder":
+                scheduleLocal(reminder: true)
+                let ac = UIAlertController(title: "Custom show identifier", message: "The user tapped the Remind me later button", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            default:
+                break
+            }
+        }
+        completionHandler()
+    }
 }
 
